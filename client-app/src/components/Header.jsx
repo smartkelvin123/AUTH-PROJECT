5d6caf0db11fd6000c76a878376110d857492f28
unknown <smartkelvin66@gmail.com>
2024-01-24 22:30:44 +0100

    completed-header

--- client-app/src/components/Header.jsx
+++ client-app/src/components/Header.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="bg-slate-200">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <h1 className="font-bold">AUth-App</h1>
+
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li>home</li>
+          </Link>
+          <Link to="/about">
+            <li>about</li>
+          </Link>
+          <Link to="sign-in">
+            <li>sign in </li>
+          </Link>
+        </ul>
+      </div>
     </div>
   );
 };
--- client-app/src/pages/About.jsx
+++ client-app/src/pages/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return <div>smart-About</div>;
 };
 
 export default About;
