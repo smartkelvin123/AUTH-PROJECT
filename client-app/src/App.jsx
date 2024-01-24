72a56ab39c6bd3a2396b7c03c1048393e7705adf
unknown <smartkelvin66@gmail.com>
2024-01-24 22:09:09 +0100

    Auth-prooject

--- client-app/src/App.jsx
+++ client-app/src/App.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-red-500">smart</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-In" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
