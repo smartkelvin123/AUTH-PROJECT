af629b838bd13d061d2c83080d1ab2f07becfec7
unknown <smartkelvin66@gmail.com>
2024-01-26 07:01:56 +0100

    conneced mongdb

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,29 @@
-import express from "express";
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const colors = require("colors");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+mongoose
+  .connect(MONGO_URI)
+  .then((conn) => {
+    console.log(
+      `MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold
+    );
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`.cyan.underline.bold);
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection failed", err);
+    // Terminate the server if the MongoDB connection fails
+    process.exit(1);
+  });
