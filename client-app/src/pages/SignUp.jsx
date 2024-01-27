f4d163c7f7f4d32f4965b1196e4cda678d3758ad
unknown <smartkelvin66@gmail.com>
2024-01-27 14:40:27 +0100

    api-route

--- client-app/src/pages/SignUp.jsx
+++ client-app/src/pages/SignUp.jsx
@@ -1,31 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      setLoading(false);
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-9">Sign up</h1>
-      <form className="flex  flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex  flex-col gap-4">
         <input
           type="text"
           placeholder="username"
+          name="username"
           id="username"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="email"
+          name="email"
           id="email"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="password"
           id="password"
+          name="password"
           className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg  uppercase hover:opacity-95 disabled:opacity-80">
-          sign up
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg  uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "loading..." : "sign up"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -34,6 +75,7 @@ const SignUp = () => {
           <span className="text-blue-500">Sign in</span>
         </Link>
       </div>
+      <p className="text-red-700 mt-5">{error && "Something went wrong!"}</p>
     </div>
   );
 };
--- server/api/controllers/auth.controller.js
+++ server/api/controllers/auth.controller.js
@@ -1,11 +1,14 @@
 const User = require("../models/user");
 const bcryptjs = require("bcryptjs");
 const errorHandler = require("../utilis/error");
+const jwt = require("jsonwebtoken");
 
 const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
-  const hashedPassword = bcryptjs.hashSync(password, 10);
+  // const hashedPassword = bcryptjs.hashSync(password, 10);
+  const salt = await bcryptjs.genSaltSync(10);
+  const hashedPassword = await bcryptjs.hashSync(req.body.password, salt);
 
   const newUser = new User({
     username: username,
@@ -21,6 +24,30 @@ const signup = async (req, res, next) => {
   }
 };
 
+const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "user not found"));
+    const validPassword = await bcryptjs.compareSync(
+      password,
+      validUser.password
+    );
+    if (!validPassword) return next(errorHandler(401, "Wrong credentails"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc; // _doc not to get more information from the database
+    const expiryDate = new Date(Date.now() + 7200000);
+    res
+      .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
+
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   signup,
+  signin,
 };
