09ee5505cccc73cc53eb30c683fc774522014d49
unknown <smartkelvin66@gmail.com>
2024-01-27 14:57:56 +0100

    complet-signin-page

--- client-app/src/pages/SignUp.jsx
+++ client-app/src/pages/SignUp.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignUp = () => {
   const [formData, setFormData] = useState({});
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -29,6 +30,7 @@ const SignUp = () => {
         setError(true);
         return;
       }
+      navigate("/sign-in");
     } catch (error) {
       setLoading(false);
       setError(true);
