de082755b0c8696974154c57709e8939d0f49a60
unknown <smartkelvin66@gmail.com>
2024-01-30 14:13:09 +0100

    add ooath-component

--- server/api/models/user.js
+++ server/api/models/user.js
@@ -15,6 +15,11 @@ const userSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
+  profilePicture: {
+    type: String,
+    default:
+      "https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg",
+  },
 });
 
 module.exports = mongoose.model("User", userSchema);
