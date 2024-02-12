f79934f6cd5b89ccb9aa595423b8ba9fed299721
unknown <smartkelvin66@gmail.com>
2024-02-12 10:01:32 +0100

    updated-about

--- client-app/src/pages/About.jsx
+++ client-app/src/pages/About.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-const About = () => {
-  return <div>smart-About</div>;
+const about = () => {
+  return (
+    <div className="px-4 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold  mb-4 text-slate-800">About</h1>
+      <p className="mb-4 text-slate-700">
+        This is a MERN (MongoDB, Express, React, Node.js) stack application with
+        authentication. It allows users to sign up, log in, and log out, and
+        provides access to protected routes only for authenticated users.
+      </p>
+    </div>
+  );
 };
 
 export default About;
